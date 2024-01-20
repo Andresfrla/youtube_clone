41e818d424388e6a5e2154aed86b7f9fb3f42850
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2024-01-20 15:48:35 -0600

    adding the title of the feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -15,6 +15,16 @@ const Feed = () => {
           Copyright 2022 Andres Franco
         </Typography>
       </Box>
+      <Box p={2} sx={{
+        overflow: 'auto',
+        height: '90vh', flex: 2
+      }}>
+        <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: 'white'}}>
+          New <span style={{color: '#F31503'}}>
+            videos
+          </span>
+        </Typography>
+      </Box>
     </Stack>
   )
 }
