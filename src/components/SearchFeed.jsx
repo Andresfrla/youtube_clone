40c50f7bed28adf5795f37b7ff3722b3e97509c7
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2024-01-21 14:09:41 -0600

    pendding the fetch on the video detail

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { Box, Typography } from '@mui/material'
+import { Videos } from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+import { useParams } from 'react-router-dom'
 
-function SearchFeed() {
+const SearchFeed = () => {
+  const [videos, setVideos] = useState([])
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+    .then((data) => setVideos(data.items))
+  },[])
+
   return (
-    <div>
-      Search Feed
-    </div>
+    <Box p={2} sx={{
+      overflow: 'auto',
+      height: '90vh', flex: 2
+    }}>
+      <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: 'white'}}>
+        Search Results for: <span style={{color: '#F31503'}}>
+          {searchTerm}
+        </span> Videos
+      </Typography>
+      <Videos videos={videos}/>
+    </Box>
   )
 }
 
 export default SearchFeed
+
